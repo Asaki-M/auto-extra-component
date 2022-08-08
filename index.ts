29a7638520248118684d05802c29742a37faf001
Qinghuan <1041220793@qq.com>
2022-08-08 22:32:37 +0800

    feat: add input imported component path and export component path

--- index.ts
+++ index.ts
@@ -1,11 +1,33 @@
 #!/usr/bin/env node
 
 import prompts from 'prompts'
-(async () => {
-  const res = await prompts({
-    type: 'number',
-    name: 'value',
-    message: 'input a number: '
-  })
-  console.log(res)
-})()
+
+async function init() {
+  let result: {
+    componentImportProject?: string
+    componentExportProject?: string
+  } = {}
+
+  try {
+    result = await prompts([
+      {
+        name: 'componentImportProject',
+        type: 'text',
+        message: 'The component entry path that needs to be imported: '
+      },
+      {
+        name: 'componentExportProject',
+        type: 'text',
+        message: 'The component export path that needs to be exported: '
+      }
+    ])
+  } catch (cancelled) {
+    console.log(cancelled.message)
+    process.exit(1)
+  }
+  console.log(result)
+}
+
+init().catch((e) => {
+  console.error(e)
+})
