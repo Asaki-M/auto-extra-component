edbdb53d7412a5983843809b954419ce687170cf
Qinghuan <1041220793@qq.com>
2022-08-11 23:05:43 +0800

    feat: add getLocalImport and getAliasConfig function

--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
 #!/usr/bin/env node
 
 import prompts from 'prompts'
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import path from 'node:path'
 
-function getAliasFromConfig(configPath) {
-  fs.readFile(configPath, (err, data) => {
-    console.log(data)
+async function getAliasFromConfig(configPath) {
+  const config = await import(configPath)
+  // get alias configs
+  const alias = config['alias']
+  if (!!alias && !!Object.keys(alias).length) {
+    console.error('Can not find alias config.')
+    process.exit(1)
+  }
+  return alias
+}
+
+async function getLocalImport(aliasConfig, exportComponentPath: string) {
+  const componentContentStr = (await fs.readFile(path.resolve(exportComponentPath))).toString()
+  const importPaths = componentContentStr
+    .split('\n')
+    .filter((item) => item.indexOf('import') !== -1)
+  const localImport = []
+  importPaths.forEach((item) => {
+    const [importName, importPath] = item.split('import')[1].split('from')
+    // compare aliasConfig has importPath, push it importName and importPath to a object in localImport
   })
-  return {}
 }
 
 async function init() {
   let inputPath: {
-    importProject?: string
-    exportProject?: string
-    exportComponentPath?: string
-  } = {}
+    importProject: string
+    exportProject: string
+    exportComponentPath: string
+  }
 
   try {
     inputPath = await prompts([
@@ -33,7 +49,7 @@ async function init() {
       {
         name: 'exportComponentPath',
         type: 'text',
-        message: 'The component path that needs to be exported: '
+        message: 'The component entry path that needs to be exported: '
       }
     ])
     const importAliasConfig = getAliasFromConfig(inputPath.importProject)
